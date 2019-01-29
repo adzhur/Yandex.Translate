f5b7d18ca28d661c58d55c732cf7159db0bbf4c2
Andrew Dzhur <a.dzhur@icloud.com>
2019-01-29 22:04:08 +0300

    Some Fix

---
+++ Translator/Translator/View/UISwitcherView.swift
@@ -0,0 +1,19 @@
+//
+//  UISwitcherView.swift
+//  Translator
+//
+//  Created by Andrew Dzhur on 29/01/2019.
+//  Copyright © 2019 Andrew Dzhur. All rights reserved.
+//
+
+import UIKit
+
+class UISwitcherView: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        self.round(radius: rect.height * 0.5)
+    }
+    
+
+}
